dce53a083c83a34c9efedbe14b3faba7247371c9
David Ethell <david.ethell@sessionradar.com>
2017-07-24 18:21:19 -0400

    Add framework target; rename main target

---
+++ KeyboardAvoiding/KeyboardAvoidingFramework/KeyboardAvoidingFramework.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAvoidingFramework.h
+//  KeyboardAvoidingFramework
+//
+//  Created by David Ethell on 7/24/17.
+//  Copyright © 2017 IdleHandsApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAvoidingFramework.
+FOUNDATION_EXPORT double KeyboardAvoidingFrameworkVersionNumber;
+
+//! Project version string for KeyboardAvoidingFramework.
+FOUNDATION_EXPORT const unsigned char KeyboardAvoidingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAvoidingFramework/PublicHeader.h>
+
+
